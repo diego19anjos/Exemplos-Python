fe48487c0ba0fd8ba7bafda01f398599f5365056
Diego Farias <diego19anjos@gmail.com>
2023-10-02 17:59:56 -0300

    CommitEstruturaCondicional

---
+++ Estrutura Condicional/Exemplo2.py
@@ -0,0 +1,15 @@
+
+saldo_disponivel = 1000.00  # Suponha que há R$ 1000 de saldo disponível
+
+# Ler o valor que o usuário deseja sacar
+valor_saque = float(input("Digite o valor que deseja sacar: R$"))
+
+# Verificar se o valor de saque é menor ou igual ao saldo disponível
+if valor_saque <= saldo_disponivel:
+    # Se o saldo for suficiente, realizar o saque
+    saldo_disponivel -= valor_saque
+    print(f"Saque de R${valor_saque:.2f} realizado com sucesso.")
+    print(f"Saldo disponível após o saque: R${saldo_disponivel:.2f}")
+else:
+    # Se o saldo for insuficiente, mostrar uma mensagem de erro
+    print("Saldo insuficiente. Não é possível realizar o saque.")
---
+++ Estrutura Condicional/Exemplo3.py
@@ -0,0 +1,22 @@
+# Ler as três notas do aluno
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calcular a média final
+media = (nota1 + nota2 + nota3) / 3
+
+# Arredondar a média para duas casas decimais
+media = round(media, 2)
+
+# Verificar a situação do aluno
+if media >= 7.0:
+    situacao = "APROVADO"
+elif media >= 6.0:
+    situacao = "RECUPERAÇÃO"
+else:
+    situacao = "REPROVADO"
+
+# Exibir a média final e a situação do aluno
+print(f"Média final: {media:.2f}")
+print(f"Situação do aluno: {situacao}")
